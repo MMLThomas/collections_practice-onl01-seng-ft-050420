e2a325ec06c41a230f04557c566dea186e8cc0fa
michael thomas <michael.thomas2014@gmail.com>
2020-05-11 10:15:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,6 +3,26 @@ def sort_array_asc(array_of_int)
 end
 
 def sort_array_desc(array_of_int)
-  array_of_int.sort.reverse
+   array_of_int.sort do |a,b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array_of_strings)
+   array_of_strings.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
 end
 
